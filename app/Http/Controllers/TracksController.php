bf274ba02eecf7ed3078d52cc8af0e55926d67fb
Tianhang Liu <tianhanl@usc.edu>
2018-02-02 00:46:02 -0800

    finish the tracks part

--- app/Http/Controllers/TracksController.php
+++ app/Http/Controllers/TracksController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TracksController extends Controller
 {
@@ -13,7 +14,20 @@ class TracksController extends Controller
      */
     public function index()
     {
-        //
+        //track name
+        // album title
+        //     artist name
+        //     price
+        $genreId = request()->query('genre');
+        $tracks = DB::table('tracks')
+            ->select('tracks.Name as trackName', 'tracks.UnitPrice', 'albums.Title as title', 'artists.Name as artistName')
+            ->join('albums', 'albums.AlbumId', '=', 'tracks.AlbumId')
+            ->join('artists', 'artists.ArtistId', '=', 'albums.ArtistId')
+            ->where('tracks.GenreId', '=', $genreId)
+            ->get();
+        return view('tracks', [
+            'tracks' => $tracks
+        ]);
     }
 
     /**
--- resources/views/tracks.blade.php
+++ resources/views/tracks.blade.php
@@ -4,19 +4,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <title>Genres</title>
+    <title>Tracks</title>
 </head>
 <body>
     <table class="table">
         <tr>
-            <th>Genre</th>
-            <th>Tracks</th>
+            <th>Track Name</th>
+            <th>Album Title</th>
+            <th>Artist Name</th>
+            <th>Price</th>
         </tr>
-        @foreach($genres as $genre)
+        @foreach($tracks as $track)
         <tr>
-            <td>{{$genre->Name}}</td>
+            <td>{{$track->trackName}}</td>
+            <td>{{$track->title}}</td>
+            <td>{{$track->artistName}}</td>
             <td>
-                <a href="/tracks/?genre={{$genre->GenreId}}">tracks</a>
+                {{$track->UnitPrice}}
             </td>
         </tr>
         @endforeach
