c3367d5e7a1134d873deaffd8805865175cfba63
Tianhang Liu <tianhanl@usc.edu>
2018-02-06 18:44:25 -0800

    modularize blade layouts

--- resources/views/playlists-list.blade.php
+++ resources/views/playlists-list.blade.php
@@ -1,20 +1,13 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <title>Playlists</title>
-</head>
-<body>
-    <ul>
-        @foreach($playlists as $playlist)
-        <li>
-            <a href="/playlists/{{$playlist->PlaylistId}}">
+@extends('main-layout') 
+@section('title', 'Playlists') 
+@section('content')
+<ul>
+    @foreach($playlists as $playlist)
+    <li>
+        <a href="/playlists/{{$playlist->PlaylistId}}">
                 {{$playlist->Name}}
             </a>
-        </li>
-        @endforeach
-    </ul>
-</body>
-</html>
+    </li>
+    @endforeach
+</ul>
+@endsection
