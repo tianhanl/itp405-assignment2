11f8e0a07289dcdbd8f43197ef8c1afeb518005e
Tianhang Liu <tianhanl@usc.edu>
2018-02-27 18:50:40 -0800

    add create review link

--- resources/views/album-reviews.blade.php
+++ resources/views/album-reviews.blade.php
@@ -1,12 +1,10 @@
 @extends('main-layout') 
 @section('title', 'Artists') 
-@section('content')
-<ul>
-    @foreach($reviews as $review)
-    <div>
-        <h2>{{$review->title}}</h2>
-        <p>{{$review->body}}</p>
-    </div>
-    @endforeach
-</ul>
+@section('content') @foreach($reviews as $review)
+<div>
+    <h2>{{$review->title}}</h2>
+    <p>{{$review->body}}</p>
+</div>
+@endforeach
+<a href="/albums/{{$id}}/reviews/new">Write a Review</a>
 @endsection
